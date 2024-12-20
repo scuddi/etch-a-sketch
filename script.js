622cb7556335fe2e60b8be8a8395c2f84e223ce8
Konstantin <157764402+scuddi@users.noreply.github.com>
2024-12-20 12:32:24 +0100

    Add id to every created cell
    Id will be variable i from for loop.

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ document.addEventListener("DOMContentLoaded", function() {
     addDivs();
 });
 
-
 function addDivs() {
     for (let i = 0; i < 256; i++) {
         var cell = document.createElement("div");
         cell.className = "cell";
+        cell.id = i;
         document.getElementById("container").appendChild(cell);
     };
 };
