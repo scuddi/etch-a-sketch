1bacd2e3f25e85734ec1592fc8c1ececaa013626
Konstantin <157764402+scuddi@users.noreply.github.com>
2024-12-27 15:17:26 +0100

    Add function addDivsCustomised and some changes
    Added the function so that when restart button is
    pressed the chosen amount of squares_per_side is
    generated and is styled when mousedover.
    
    Had to do some minor changes to the restartSite
    function too, so multiple iterations of restarts
    work.

--- script.js
+++ script.js
@@ -6,17 +6,19 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function addDivs() {
     for (let i = 0; i < 256; i++) {
-        var cell = document.createElement("div");
+        let cell = document.createElement("div");
         cell.className = "cell";
         cell.id = i;
         document.getElementById("container").appendChild(cell);
-        cell.setAttribute("onmouseover", `paintDivs(${i})`)
+        cell.setAttribute("onmouseover", `paintDivs(${i})`);
     };
 };
 
+// Paint divs while mouseover
+
 function paintDivs(i) {
     document.getElementById(i).style.background = "blue";
-}
+};
 
 // Function for user prompt and to restart the entire site
 
@@ -24,4 +26,21 @@ function restartSite() {
     let numberOfSquaresPerSide = prompt("How many squares should each side of your Etch-A-Sketch have? 100 is the maximum", 100);
     alert(`You chose ${numberOfSquaresPerSide}`);
     document.querySelectorAll(".cell").forEach(e => e.remove());
+    document.querySelectorAll(".cell_custom").forEach(e => e.remove());
+    addDivsCustomised(numberOfSquaresPerSide);
+};
+
+// Function for when site is restarted through button and user chooses amount of squares
+
+function addDivsCustomised(numberOfSquaresPerSide) {
+    for (let i = 0; i < numberOfSquaresPerSide * numberOfSquaresPerSide; i++) {
+        let cell_custom = document.createElement("div");
+        cell_custom.className = "cell_custom";
+        cell_custom.id = i;
+        document.getElementById("container").appendChild(cell_custom);
+        cell_custom.setAttribute("onmouseover", `paintDivs(${i})`);
+
+        let cell_custom_width_height = 100 / numberOfSquaresPerSide;
+        cell_custom.setAttribute("style", `width:${cell_custom_width_height}%; height:${cell_custom_width_height}%`);
+    };
 }
